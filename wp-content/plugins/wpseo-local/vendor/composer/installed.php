69316351b5a75dc6073c934ded9a72e5b6f90fee
Jorge Sierra <jorgemaikelsierra@outlook.com>
2022-12-28 09:40:51 -0500

    Instalacion plugin wordpress-local-seo-premium

---
+++ wp-content/plugins/wpseo-local/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'aeb62c44d49982d96c1a9035e149d8a98ce1aa0b',
+        'name' => 'yoast/wordpress-seo-local',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'yoast/i18n-module' => array(
+            'pretty_version' => '3.1.1',
+            'version' => '3.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yoast/i18n-module',
+            'aliases' => array(),
+            'reference' => '9d0a2f6daea6fb42376b023e7778294d19edd85d',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo-local' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'aeb62c44d49982d96c1a9035e149d8a98ce1aa0b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
